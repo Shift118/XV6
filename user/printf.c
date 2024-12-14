9740084bf9a9eefad945972bea456ea21741589b
Shift118 <mostafayounes013@hotmail.com>
2024-12-14 22:21:46 +0200

    Riscv-Assembly , Backtrace from the trap lab

--- user/printf.c
+++ user/printf.c
@@ -1,7 +1,6 @@
 #include "kernel/types.h"
 #include "kernel/stat.h"
 #include "user/user.h"
-
 #include <stdarg.h>
 
 static char digits[] = "0123456789ABCDEF";
@@ -155,3 +154,6 @@ printf(const char *fmt, ...)
   va_start(ap, fmt);
   vprintf(1, fmt, ap);
 }
+
+
+
